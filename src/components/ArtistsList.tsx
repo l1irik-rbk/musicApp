a250834dfa1e647861c7350635c3c36e519a6f0b
l1irik-rbk <kir.baranow2010@gmail.com>
2022-12-19 19:56:04 +0300

    feat: add artists styles

--- src/components/ArtistsList.tsx
+++ src/components/ArtistsList.tsx
@@ -1,17 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 import { IArtistA } from '../helpers/constantsTypes';
+
+const StyledTrack = styled.div`
+  border: 1px solid hsl(207, 26%, 17%);
+  border-radius: 1rem;
+  padding: 10px;
+  width: 300px;
+
+  @media (min-width: 767px) {
+    width: 500px;
+  }
+`;
+
+const Title = styled.h3`
+  display: inline-block;
+  margin-bottom: 10px;
+  transition: all 0.5s;
+
+  &:hover {
+    color: hsl(0deg 1% 60%);
+  }
+
+  @media (min-width: 767px) {
+    font-size: 25px;
+  }
+`;
 
 const ArtistsList = ({ artists }: { artists: IArtistA[] }): JSX.Element => {
   return (
     <>
       {artists.map(({ artist }) => (
-        <div key={artist.artist_id}>
+        <StyledTrack key={artist.artist_id}>
           <Link to={`/artists/${artist.artist_id}`}>
-            <h3>{artist.artist_name}</h3>
+            <Title>{artist.artist_name}</Title>
           </Link>
-        </div>
+        </StyledTrack>
       ))}
     </>
   );
--- src/components/TracksList.tsx
+++ src/components/TracksList.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+
 import { ITrackA } from '../helpers/constantsTypes';
 import MusicLinks from './MusicLinks';
 
